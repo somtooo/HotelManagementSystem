85498a69db8d5973063f6326ac03139d2b6f1443
Somtoo <you@example.com>
2019-11-25 21:54:27 -0600

    Initial commit

---
+++ src/splashScreen/splashScreenController.java
@@ -0,0 +1,13 @@
+package splashScreen;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class splashScreenController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
